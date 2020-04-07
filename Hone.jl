97c12f39e8b801c5be79b3781f1abeeb33b9b2b6
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-04-07 11:51:13 -0400

    Updated with hone module, packaged.

--- Hone.jl
+++ Hone.jl
@@ -1,3 +1,10 @@
+
+#==
+       Hone.jl
+    Emmett Boudreau
+    April 7th, 2020
+==#
+module Hone
 using Compose
 tree(b) = introspect(b.composition)
 
@@ -13,4 +20,7 @@ function Circle(diameter,x,y,fillin)
     composition = compose(context(), circle(x,y,diameter), fill(fillin))
     show() = composition
     (test)->(show;composition)
+end
+function polygon()
+end
 end
