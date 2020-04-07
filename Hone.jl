98be4382e81ad0f23dabb74fee7ba384ddc64ce4
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-04-07 10:34:45 -0400

    Init commit

---
+++ Hone.jl
@@ -0,0 +1,12 @@
+using Compose
+tree(b) = introspect(b.img)
+function Donut(data)
+    predict(xt) = (xt = [i = a + (b * i) for i in xt])
+    show() = img
+    (test)->(variable;variable;predict)
+end
+function Pie(data)
+    predict(xt) = (xt = [i = a + (b * i) for i in xt])
+    show() = img
+    (test)->(variable;variable;predict)
+end
