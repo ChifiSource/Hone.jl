1201135d786776806ca584ab3fc48a214c355ff1
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-04-10 15:59:10 -0400

    Added Circle object and Scatter with meta

--- src/Hone.jl
+++ src/Hone.jl
@@ -6,29 +6,30 @@
 ==#
 module Hone
 using Compose
-tree(b) = introspect(b.composition)
-
-function Figure()
-
-end
-function textbox(value)
-    composition = compose(context(), text(0.5, 0.5,value))
-    show() = composition
-    (test)->(show;composition)
-end
-function Circle(diameter,x,y,fillin)
+function Circle(x,y,diameter,fillin)
+    bravo = false
     composition = compose(context(), circle(x,y,diameter), fill(fillin))
+   #     "compose(context(),circle(.5,.5,.1))
+    tag = string("circle(",string(x),", ",string(y),", ",string(diameter),")")
+    tree() = introspect(composition)
     show() = composition
-    (test)->(show;composition)
-end
-function polygon()
+    x = x
+    y = y
+    update(x,y) =  bravo = true;tag = string("circle(",string(x),", ",string(y),", ",diameter,")"); if bravo == true return(tag); bravo = false end
+    (var)->(show;composition;tree;tag;x;y;update;diameter)
 end
 function Scatter(x,y,shape)
-    top = maximum(y)
-    bottom = minimum(x)
-    
-end
-
+   topx = maximum(x)
+    topy = maximum(y)
+    x = [z = z / topx for z in x]
+    y = [z = z / topx for z in x]
+    expression = string("compose(context(),")
+    for (i, w) in zip(x, y)
+        exp = shape.update(i,w)
+        expression = string(expression,string(exp))
+    end
+    expression = Meta.parse(string(expression, ")"))
+    eval(expression)
 end
 # =====================================
 end
