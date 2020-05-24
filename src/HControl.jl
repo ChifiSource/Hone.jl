dcad2fe1e53b0339d17a7cbfe6c71b93e53842c4
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-05-24 15:00:21 -0400

    added frame adding

--- src/HControl.jl
+++ src/HControl.jl
@@ -7,20 +7,24 @@ function Frame(width, height, lm, rm, tm, bm)
     tm, ",", bm,
         ")),")
     objects = []
+    tag = base
     composition = nothing
-    add(objects) = composition,objects,tag = _frameup(base,objects)
+    add(object) = composition,objects,tag = _frameup(base,objects,object)
     show() = composition
     tree() = introspect(composition)
     save(name) = draw(SVG(name), composition)
     (var)->(add;show;tag;base;objects;composition;save;tree;width;height;lm;bm;rm;tm)
 end
-function _frameup(tag,objects)
-    for object in objects
-       tag = string(tag,"(context(),",object.tag, ")")
+function _frameup(tag,objects,object)
+    push!(objects, object.tag)
+    objecttags = ""
+    for o in objects
+       objecttags = string(objecttags,"(context(),",o, "),")
     end
-    tag = string(tag,")")
-    println(tag)
+    println("objects: ",objecttags)
+    tag = string(tag,objecttags,")")
     exp = Meta.parse(tag)
+    println(exp)
     composition = eval(exp)
     return(composition,objects,tag)
 end
